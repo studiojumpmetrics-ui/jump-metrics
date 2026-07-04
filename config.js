54f6c210c9e39243a1b7da17d5e95369b377746e
studiojumpmetrics-ui <studiojumpmetrics@gmail.com>
2026-07-04 17:32:14 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+/*
+  CONFIGURAÇÃO DO JUMP METRICS
+  Cole aqui somente a Project URL e a Publishable Key do Supabase.
+  Não use a Secret Key, service_role ou senha do banco.
+*/
+window.JUMP_METRICS_CONFIG = {
+  SUPABASE_URL: 'https://biepldngfwtsohwadpeb.supabase.co/rest/v1/',
+  SUPABASE_PUBLISHABLE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJpZXBsZG5nZnd0c29od2FkcGViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMxODQzMjAsImV4cCI6MjA5ODc2MDMyMH0.VZdefz1mVkSIi2yGmA_-xTuTZIu1Cpwk4v0ZF3FEj_U',
+};
