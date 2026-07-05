6327b410b47776330536657bb8d1a74430ce6000
studiojumpmetrics-ui <studiojumpmetrics@gmail.com>
2026-07-05 15:32:48 -0300

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,6 @@
   Não use a Secret Key, service_role ou senha do banco.
 */
 window.JUMP_METRICS_CONFIG = {
-  SUPABASE_URL: 'https://biepldngfwtsohwadpeb.supabase.co/rest/v1/',
+  SUPABASE_URL: 'https://biepldngfwtsohwadpeb.supabase.co',
   SUPABASE_PUBLISHABLE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJpZXBsZG5nZnd0c29od2FkcGViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMxODQzMjAsImV4cCI6MjA5ODc2MDMyMH0.VZdefz1mVkSIi2yGmA_-xTuTZIu1Cpwk4v0ZF3FEj_U',
 };
