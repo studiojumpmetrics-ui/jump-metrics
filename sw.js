4d582595c71f3adb1ab33f1dd97dbc051b23f4bf
studiojumpmetrics-ui <studiojumpmetrics@gmail.com>
2026-07-23 09:48:46 -0300

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE='jump-metrics-v7';
-const ASSETS=['./','./index.html','./app.css','./app.js','./jump-metrics-logo.png','./jump-metrics-hero.png','./manifest.webmanifest'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET') return;
-  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
-});
